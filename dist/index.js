f21f87d0758e361b50521d98e9cca057b512b988
Kevin Nagurski <kevin.nagurski@harness.io>
2022-03-14 12:40:51 +0000

    v1.0.0

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MyComponent = ({ children, heading }) => {
+  const color = 'red'
+
+  return (
+    <section style={{ borderRadius: '1rem', border: `2px solid ${color}` }}>
+      <header style={{ backgroundColor: color, padding: '1rem' }}>
+        <h1 style={{ color: 'white', fontSize: '2rem', margin: 0 }}>
+          {heading}
+        </h1>
+      </header>
+      <div style={{ padding: '1rem' }}>{children}</div>
+    </section>
+  )
+}
+
+export default MyComponent
